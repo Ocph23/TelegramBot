eec13f1fcb5de77b3cf3b9c310eebd452cc28e02
ocph23 <ocph23@gmail.com>
2019-06-29 23:13:48 +0900

    Test

---
+++ TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Ocph.Bots;
+using System;
+
+namespace TestApp
+{
+   public class Program
+    {
+        public static Bot bot { get; private set ; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            bot = new Bot("785057197:AAFEFTwd1SK3Whvw4a506YY7Bocu9dLK3F4");
+            Console.Read();
+        }
+    }
+}
